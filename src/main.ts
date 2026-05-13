c386443bd8edcc11924391a80401e53950f05c79
hectordev4 <hectordev4.services@gmail.com>
2026-05-13 12:01:09 +0200

    feat!: Footer detached from Home page

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css';
 import { setupHome } from './pages/Home';
+import { setupFooter } from './components/footer';
 
 // Use a simple conditional check (Type Guard)
 const appElement = document.getElementById('app');
@@ -9,6 +10,7 @@ if (appElement) {
   appElement.innerHTML = ''; 
   // Append the new structure
   appElement.appendChild(setupHome());
+  appElement.appendChild(setupFooter());
 } else {
   // Helpful developer warning if the HTML isn't set up right
   console.error('Target element #app was not found in the DOM.');
